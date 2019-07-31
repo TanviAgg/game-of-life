c7e260430d7afd06f44fa1b58917a3db0ebbb329
tanvi aggarwal <tanvi.aggarwal@SW-LP09371.local>
2019-08-01 00:21:04 +0530

    Test cases using FileReader

--- test/GameOfLifeTest.java
+++ test/GameOfLifeTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameOfLifeTest {
     @Test
@@ -41,69 +42,51 @@ class GameOfLifeTest {
     void shouldReturnSameForStillLifeTest1(){
         Grid grid = new Grid(3, 3);
         GameOfLife game = new GameOfLife(grid);
-        List<Pair<Integer, Integer>> seed = new ArrayList<Pair<Integer, Integer>>();
-        seed.add(new Pair<Integer, Integer>(1,1));
-        seed.add(new Pair<Integer, Integer>(1,2));
-        seed.add(new Pair<Integer, Integer>(2,1));
-        seed.add(new Pair<Integer, Integer>(2,2));
+        List<Pair<Integer, Integer>> seed = GameOfLifeFileReader.readInputFromFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Testcase1.txt");
 
         game.seedPopulation(seed);
         game.computeNextGeneration();
         List<Pair<Integer, Integer>> liveCells = game.getLiveCellsInCurrentState();
 
-        assertFalse(grid.isCellAlive(0, 0));
+        assertTrue(GameOfLifeFileReader.matchOutputWithFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Solution1.txt", liveCells));
     }
 
     @Test
     void shouldReturnSameForStillLifeTest2(){
         Grid grid = new Grid(3, 3);
         GameOfLife game = new GameOfLife(grid);
-        List<Pair<Integer, Integer>> seed = new ArrayList<Pair<Integer, Integer>>();
-        seed.add(new Pair<Integer, Integer>(0,1));
-        seed.add(new Pair<Integer, Integer>(1,0));
-        seed.add(new Pair<Integer, Integer>(2,1));
-        seed.add(new Pair<Integer, Integer>(0,2));
-        seed.add(new Pair<Integer, Integer>(1,2));
+        List<Pair<Integer, Integer>> seed = GameOfLifeFileReader.readInputFromFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Testcase2.txt");
 
         game.seedPopulation(seed);
         game.computeNextGeneration();
         List<Pair<Integer, Integer>> liveCells = game.getLiveCellsInCurrentState();
 
-        assertFalse(grid.isCellAlive(0, 0));
+        assertTrue(GameOfLifeFileReader.matchOutputWithFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Solution2.txt", liveCells));
     }
 
     @Test
     void shouldOscillateForBlinkerTest3(){
         Grid grid = new Grid(3, 3);
         GameOfLife game = new GameOfLife(grid);
-        List<Pair<Integer, Integer>> seed = new ArrayList<Pair<Integer, Integer>>();
-        seed.add(new Pair<Integer, Integer>(1,1));
-        seed.add(new Pair<Integer, Integer>(1,0));
-        seed.add(new Pair<Integer, Integer>(1,2));
+        List<Pair<Integer, Integer>> seed = GameOfLifeFileReader.readInputFromFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Testcase3.txt");
 
         game.seedPopulation(seed);
         game.computeNextGeneration();
         List<Pair<Integer, Integer>> liveCells = game.getLiveCellsInCurrentState();
 
-        assertFalse(grid.isCellAlive(0, 0));
+        assertTrue(GameOfLifeFileReader.matchOutputWithFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Solution3.txt", liveCells));
     }
 
     @Test
     void shouldOscillateForToadTest4(){
         Grid grid = new Grid(5, 5);
         GameOfLife game = new GameOfLife(grid);
-        List<Pair<Integer, Integer>> seed = new ArrayList<Pair<Integer, Integer>>();
-        seed.add(new Pair<Integer, Integer>(1,1));
-        seed.add(new Pair<Integer, Integer>(1,2));
-        seed.add(new Pair<Integer, Integer>(1,3));
-        seed.add(new Pair<Integer, Integer>(2,2));
-        seed.add(new Pair<Integer, Integer>(2,3));
-        seed.add(new Pair<Integer, Integer>(2,4));
+        List<Pair<Integer, Integer>> seed = GameOfLifeFileReader.readInputFromFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Testcase4.txt");
 
         game.seedPopulation(seed);
         game.computeNextGeneration();
         List<Pair<Integer, Integer>> liveCells = game.getLiveCellsInCurrentState();
 
-        assertFalse(grid.isCellAlive(0, 0));
+        assertTrue(GameOfLifeFileReader.matchOutputWithFile("/Users/tanvi.aggarwal/IdeaProjects/GameOfLife/test/Solution4.txt", liveCells));
     }
 }
